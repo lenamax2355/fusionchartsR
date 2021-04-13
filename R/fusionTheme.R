8924ed6b128cd15a6786f666829a69e41785c715
Alex Yahiaoui Martinez <40006040+alexym1@users.noreply.github.com>
2021-04-13 12:57:55 +0200

    Update fusionTheme.R

--- R/fusionTheme.R
+++ R/fusionTheme.R
@@ -7,16 +7,13 @@
 #' @param fusionPlot fusionPlot object got by \code{fusionPlot()}
 #' @param theme Chart theme
 #' 
-
+#' 
 #' @examples
 #' library(fusionchartsR)
-#' 
 #' df <- data.frame(label = c("Venezuela", "Saudi", "Canada", "Russia"), value = c(290, 260,180, 115))
-#' fusionPlot(data = df, type = "pie2d") %>%
-#'   fusionTheme(theme = "fusion")
-#'    
-#' fusionPlot(data = df, type = "column2d") %>%
-#'   fusionTheme(theme = "gammel")
+#' df %>%
+#' fusionPlot(x = "label", y = "value", type = "pie2d") %>%
+#' fusionTheme(theme = "gammel")
 #' 
 #'
 #' @export
