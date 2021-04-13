d7b9f3a4667a7935912cf216ca390f696260fb26
Alex Yahiaoui Martinez <40006040+alexym1@users.noreply.github.com>
2021-04-13 12:58:15 +0200

    Update fusionTooltip.R

--- R/fusionTooltip.R
+++ R/fusionTooltip.R
@@ -13,11 +13,11 @@
 #' 
 #' @examples
 #' library(fusionchartsR)
-#' 
 #' df <- data.frame(label = c("Venezuela", "Saudi", "Canada", "Russia"), value = c(290, 260,180, 115))
-#' fusionPlot(data = df, type = "column2d") %>%
-#'   fusionTooltip(toolTipBgColor = "#3526ad", toolTipBgAlpha = "50",  showToolTipShadow = FALSE) %>%
-#'   fusionTheme(theme = "fusion")
+#' df %>%
+#' fusionPlot(x = "label", y = "value", type = "doughnut2d") %>%
+#' fusionTooltip(toolTipBgColor = "#3526ad", toolTipBgAlpha = "50",  showToolTipShadow = FALSE) %>%
+#' fusionTheme(theme = "fusion")
 #'
 #' @export
 fusionTooltip <- function(fusionPlot, showToolTip = TRUE, toolTipBorderColor = "#666666", 
