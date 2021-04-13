2b33274d8ab534429dbb162cfde054244040bc88
Alex Yahiaoui Martinez <40006040+alexym1@users.noreply.github.com>
2021-04-13 12:53:54 +0200

    Update fusionCustomLegend.R

--- R/fusionCustomLegend.R
+++ R/fusionCustomLegend.R
@@ -33,11 +33,11 @@
 #' 
 #' @examples
 #' library(fusionchartsR)
-#'
 #' df <- data.frame(label = c("Venezuela", "Saudi", "Canada", "Russia"), value = c(290, 260,180, 115))
-#' fusionPlot(data = df, type = "doughnut2d") %>%
-#'   fusionCustomLegend(plotHighlightEffect = TRUE) %>%
-#'   fusionTheme(theme = "fusion")
+#' df %>%
+#' fusionPlot(x = "label", y = "value", type = "doughnut2d") %>%
+#' fusionCustomLegend(plotHighlightEffect = TRUE) %>%
+#' fusionTheme(theme = "fusion")
 #'
 #' @export
 fusionCustomLegend <- function(fusionPlot, plotHighlightEffect = FALSE, plotHighlightEffectColor = "#7f7f7f", plotHighlightEffectAlpha = "60",
